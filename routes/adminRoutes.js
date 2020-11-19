7cae98c75bd00871f4c49a10f755b21d6faaefd3
unknown <dhruvsharma.ynr@gmail.com>
2020-11-19 13:01:12 +0530

    UI Changes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,8 +4,34 @@ const Routes = express.Router();
 
 
 Routes.get("/admin" , (req,res,next) => {
-    res.render("adminView/admin");
+    res.render("adminView/admin",{ pageTitle : 'Dashboard'});
 })
 
+Routes.get("/addnumbers" , (req,res,next) => {
+    res.render("adminView/addNumbers",{ pageTitle : 'Add Numbers'});
+})
+
+Routes.get("/viewnumbers" , (req,res,next) => {
+    res.render("adminView/viewNumbers",{ pageTitle : 'View Numbers'});
+})
+Routes.get("/updatenumbers" , (req,res,next) => {
+    res.render("adminView/updateNumbers",{ pageTitle : 'Update Numbers'});
+})
+
+Routes.get("/changepassword" , (req,res,next) => {
+    res.render("adminView/changePassword",{ pageTitle : 'Change Password'});
+})
+
+Routes.get("/viewvendors" , (req,res,next) => {
+    res.render("adminView/viewVendors",{ pageTitle : 'View Vendors'});
+})
+
+Routes.get("/addvendors" , (req,res,next) => {
+    res.render("adminView/addVendors",{ pageTitle : 'Add Vendors'});
+})
+
+Routes.get("/buyerlist" , (req,res,next) => {
+    res.render("adminView/buyerList",{ pageTitle : 'Buyers List'});
+})
 
 module.exports = Routes;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,16 +3,21 @@ const express = require("express");
 const Routes = express.Router();
 
 Routes.get("/" , (req,res,next) => {
-    res.render("userView/index")
+    res.render("userView/index",{ pageTitle : 'Fancy Number Zone'})
 });
 
 Routes.get("/categories" , (req,res,next) => {
-    res.render("userView/categories")
+    res.render("userView/categories",{ pageTitle : 'Categories'})
 });
 
 Routes.get("/contactUs" , (req,res,next) => {
     console.log(req.body);
-    res.render("userView/contactUs")
+    res.render("userView/contactUs",{ pageTitle : 'Contact Us'})
+})
+
+Routes.get("/login" , (req,res,next) => {
+    console.log(req.body);
+    res.render("userView/login",{ pageTitle : 'Log-In'})
 })
 
 // Routes.get("/cart" , (req,res,next) => {
